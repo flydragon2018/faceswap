38227c51a8ca74e86bf06477ed89422823ae47ea
flydragon2018 <37927733+flydragon2018@users.noreply.github.com>
2023-07-28 11:34:55 +0800

    Add files via upload

--- faceSwap_demo.py
+++ faceSwap_demo.py
@@ -4,6 +4,9 @@ import sys
 import numpy as np
 import cv2
 
+import dlib
+
+
 # Read points from text file
 def readPoints(path) :
     # Create an array of points.
@@ -123,7 +126,53 @@ def warpTriangle(img1, img2, t1, t2) :
     img2[r2[1]:r2[1]+r2[3], r2[0]:r2[0]+r2[2]] = img2[r2[1]:r2[1]+r2[3], r2[0]:r2[0]+r2[2]] + img2Rect 
     
 
+
+
+def get_face68_txt(face_img): 
+    image = cv2.imread(face_img)
+    # Convert the image color to grayscale
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    # Detect the face
+    rects = detector(gray, 1)
+    # Detect landmarks for each face
+    for rect in rects:
+        # Get the landmark points
+        shape = predictor(gray, rect)
+	# Convert it to the NumPy Array
+        shape_np = np.zeros((68, 2), dtype="int")
+        for i in range(0, 68):
+            shape_np[i] = (shape.part(i).x, shape.part(i).y)
+        shape = shape_np
+
+        # Display the landmarks
+        #for i, (x, y) in enumerate(shape):
+	    # Draw the circle to mark the keypoint 
+        #    cv2.circle(image, (x, y), 1, (0, 0, 255), -1)
+		
+    # Display the image
+    #cv2.imshow('Landmark Detection', image)
+    
+    # Displaying the array
+    print('face landmarks:\n', shape_np)
+    fn=str(face_img)
+    #lms_fn=fn[:fn.rindex('.')]+'.txt'
+    lms_fn=fn+'.txt'
+    file = open(lms_fn, "w")
+ 
+    # Saving  face landmarks  in a text file
+    # 1-68  x  y format
+    for i in range(len(shape_np)):
+        #file.write('{0} {1} {2}\n'.format(i+1, shape_np[i][0],shape_np[i][1]))    
+        file.write('{0} {1}\n'.format(shape_np[i][0],shape_np[i][1]))      
+    file.close()
+
+
+
+
 if __name__ == '__main__' :
+
+    detector = dlib.get_frontal_face_detector()
+    predictor = dlib.shape_predictor('shape_predictor_68_face_landmarks.dat')
     
     # Make sure OpenCV is version 3.0 or above
     (major_ver, minor_ver, subminor_ver) = (cv2.__version__).split('.')
@@ -133,11 +182,17 @@ if __name__ == '__main__' :
         sys.exit(1)
 
     # Read images
-    filename1 = 'ted_cruz.jpg'
-    filename2 = 'donald_trump.jpg'
+    #filename1 = 'ted_cruz.jpg'
+    #ilename2 = 'donald_trump.jpg'
     
-    #filename1 = 'source1.jpg'
-    #filename2 = 'source2.jpg'
+    filename1 = 'source1.jpg'
+    filename2 = 'source2.jpg'
+
+    get_face68_txt(filename1)
+
+    get_face68_txt(filename2)
+
+
 
     img1 = cv2.imread(filename1);
     img2 = cv2.imread(filename2);
@@ -196,6 +251,15 @@ if __name__ == '__main__' :
     
     # Clone seamlessly.
     output = cv2.seamlessClone(np.uint8(img1Warped), img2, mask, center, cv2.NORMAL_CLONE)
+
+    #modify window to adapt too big file
+
+    cv2.imwrite( '{0}_{1}_swap.jpg'.format(filename1,filename2),output)
+
+    width, height=output.shape[:2]
+    cv2.namedWindow('Face Swapped', cv2.WINDOW_NORMAL)
+    cv2.resizeWindow('Face Swapped', width, height)
+
     
     cv2.imshow("Face Swapped", output)
     cv2.waitKey(0)
